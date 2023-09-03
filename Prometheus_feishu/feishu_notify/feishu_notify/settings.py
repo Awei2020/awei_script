adc557bb57e93b3637c374335f38bf786b761a54
aweihooo <89605281@qq.com>
2023-09-03 18:03:09 +0800

    fix:time

--- Prometheus_feishu/feishu_notify/feishu_notify/settings.py
+++ Prometheus_feishu/feishu_notify/feishu_notify/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-3%cqxzd_^(f%!90_^2ds)m&p@7902oi&y14w21b^2whm2a+)#2
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -43,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -69,7 +67,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'feishu_notify.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -79,7 +76,6 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -99,19 +95,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-#语言中文
+# 语言中文
 LANGUAGE_CODE = 'zh-Hans'
-#设置时区
+# 设置时区
 TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- Prometheus_feishu/feishu_notify/feishu_notify/views.py
+++ Prometheus_feishu/feishu_notify/feishu_notify/views.py
@@ -1,19 +1,24 @@
 # 设置webhook路由
 # Path: feishu_notify\feishu_notify\views.py
-from django.http import HttpResponse
-import os
-import json
-import requests
 import datetime
-date_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+import json
+import os
+
+import requests
+from django.http import HttpResponse
+
 feishu_webhook_url = os.environ.get('FEISHU_WEBHOOK_URL')
 print(feishu_webhook_url)
 headers = {"Content-Type": "application/json;charset=utf-8"}
+
+
 def webhook(request):
     if request.method == 'POST':
+        date_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         data = json.loads(request.body.decode('utf-8'))["alerts"]
         title = data[0]["labels"]["instance"] + "发生异常"
-        msg = "异常信息：" + data[0]["labels"]["alertname"] + "\n" + "异常级别：" + data[0]["labels"]["severity"] + "\n" + "异常描述：" + data[0]["annotations"]["description"] + "\n" + "异常时间：" + date_time
+        msg = "异常信息：" + data[0]["labels"]["alertname"] + "\n" + "异常级别：" + data[0]["labels"][
+            "severity"] + "\n" + "异常描述：" + data[0]["annotations"]["description"] + "\n" + "异常时间：" + date_time
         print(title, msg)
         msg_body = {
             "msg_type": "post",
